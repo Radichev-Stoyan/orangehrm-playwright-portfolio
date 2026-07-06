f2befba96ad0abaee716629e327ccb370c577614
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2026-07-06 10:28:14 +0300

    Adding an employee without a last name

--- tests/pim/employee.spec.ts
+++ tests/pim/employee.spec.ts
@@ -52,4 +52,25 @@ test.describe('PIM - Employee management', () => {
 
         await expect(page.getByText('Required')).toBeVisible();
     });
+
+    test.only('Adding an employee without a last name', async ({ page }, testInfo) => {
+        const menu = new SideMenu(page, testInfo.project.name);
+        const employee = randomEmployee();
+
+        await menu.openPim();
+
+        await expect(page.getByRole('button', { name: ' Add ' })).toBeVisible();
+
+        await page.getByRole('button', { name: ' Add ' }).click();
+
+        await expect(page.getByRole('heading', { name: 'Add Employee' })).toBeVisible();
+
+        await page.getByPlaceholder('First Name').fill(employee.firstName);
+        await page.getByPlaceholder('Middle Name').fill(employee.middleName);
+        await page.locator('.oxd-input--active').last().fill(employee.employeeId);
+
+        await page.getByRole('button', { name: ' Save ' }).click();
+
+        await expect(page.getByText('Required')).toBeVisible();
+    });
 })
