cc9dbfad98e34cae5bfc9de00c4a51a6fa4c2e74
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2026-07-06 10:25:00 +0300

    Adding an employee without a first name

--- tests/pim/employee.spec.ts
+++ tests/pim/employee.spec.ts
@@ -30,5 +30,26 @@ test.describe('PIM - Employee management', () => {
         const employeeFullName = `${employee.firstName} ${employee.lastName}`;
 
         await expect(page.getByRole('heading', { name: employeeFullName })).toBeVisible({ timeout: 15000 });
-    })
+    });
+
+    test('Adding an employee without a first name', async ({ page }, testInfo) => {
+        const menu = new SideMenu(page, testInfo.project.name);
+        const employee = randomEmployee();
+
+        await menu.openPim();
+
+        await expect(page.getByRole('button', { name: ' Add ' })).toBeVisible();
+
+        await page.getByRole('button', { name: ' Add ' }).click();
+
+        await expect(page.getByRole('heading', { name: 'Add Employee' })).toBeVisible();
+
+        await page.getByPlaceholder('Middle Name').fill(employee.middleName);
+        await page.getByPlaceholder('Last Name').fill(employee.lastName);
+        await page.locator('.oxd-input--active').last().fill(employee.employeeId);
+
+        await page.getByRole('button', { name: ' Save ' }).click();
+
+        await expect(page.getByText('Required')).toBeVisible();
+    });
 })
